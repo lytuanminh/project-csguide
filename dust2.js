7e6399c0f04c602b084b01837a378208431273e4
unknown <lytuanminh122@gmail.com>
2019-08-14 11:54:03 +0700

    first commit

---
+++ dust2.js
@@ -0,0 +1,35 @@
+dust2.addEventListener('click', () => {
+    let dust2Content = `<div>
+    <a href="#d2smoke" id="d2smoke" onclick='videos()'>Smoke</a>
+    <a href="#d2fl" id="d2flash">Flash</a>
+    <a href="#d2moly" id="d2moly">Moly</a>
+    <a href="#d2bonus" id="d2bonus">Bonus</a>
+    </div>
+    
+    <div id="vid-container">
+    </div>`
+    pageContainer.innerHTML = dust2Content;
+    pageContainer.style.backgroundColor = "bisque";
+})
+
+let d2Smoke = document.getElementById('d2smoke');
+
+// d2Smoke.addEventListener('click', () => {
+//     let divVideo = document.getElementById('vid-container');
+//     let videos = `
+//     <video width="320" height="240" controls>
+//         <source src="https://giant.gfycat.com/FlamboyantIllfatedKitten.mp4" type="video/mp4">
+//     </video>
+//     `
+//     divVideo.innerHTML += videos;
+// })
+
+function videos(){
+    let divVideo = document.getElementById('vid-container');
+    let videos = `
+    <video width="320" height="240" controls>
+        <source src="https://giant.gfycat.com/FlamboyantIllfatedKitten.mp4" type="video/mp4">
+        </video>
+        `
+    divVideo.innerHTML = videos;
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+let pageContainer = document.getElementById('container');
+let dust2 = document.getElementById('dust2');
+let mirage = document.getElementById('mirage');
+let cache = document.getElementById('cache');
+let inferno = document.getElementById('inferno');
+let overpass = document.getElementById('overpass');
+let nuke = document.getElementById('nuke');
+let train = document.getElementById('train');
+let vertigo = document.getElementById('vertigo');
+
+console.dir(pageContainer)
+
+
